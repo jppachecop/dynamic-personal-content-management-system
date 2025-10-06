85d2b8900a0dca0ce0a7d4dd1019733a83314f76
jppachecop <jppacheco09@hotmail.com>
2025-10-06 13:11:24 -0300

    refactor: move layout components from ui/ to layout/ directory and rename files to follow kebab-case

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import NotFound from "./pages/NotFound";
-import { WelcomeScreen } from "./components/ui/WelcomeScreen";
+import { WelcomeScreen } from "./components/layout/WelcomeScreen";
 import { AuthenticatedLayout } from "./components/layout/AuthenticatedLayout";
 import { UnauthenticatedLayout } from "./components/layout/UnauthenticatedLayout";
 import { AppLayout } from "./components/layout/AppLayout";
